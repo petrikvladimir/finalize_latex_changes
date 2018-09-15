c7c1d144e9ecbdc20854846a1920ddc8cbcf94d3
Vladimir Petrik <vladko.petrik@gmail.com>
2018-09-15 19:48:47 +0300

    refactoring of the library, mostly splitting into subfunctions.

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,9 @@ use failure::Error;
 use std::path::Path;
 use std::fs::File;
 
+#[cfg(test)]
+mod tests;
+
 enum Steps {
     DeleteUntilOpeningBracket,
     DeleteUntilClosingBracket,
@@ -22,69 +25,94 @@ pub struct Filter {
 }
 
 impl Filter {
+    /// Create a filter instance.
     pub fn new() -> Filter {
         Filter { ..Default::default() }
     }
 
+    /// Return an string with all 'changes' artifacts removed from the input text.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let mut f = finalize_latex_changes::Filter::new();
+    /// let out = f.process("\\added{asdf}");
+    /// assert_eq!(out, "asdf");
+    /// ```
     pub fn process(&mut self, text: &str) -> String {
         let mut out = String::with_capacity(text.len());
-
         for (i, c) in text.chars().enumerate() {
             let step = self.reversed_steps.pop();
             match step {
                 None => {
-                    if text[i..].starts_with("\\added") {
-                        self.counter_added += 1;
-                        self.reversed_steps.push(Steps::KeepUntilClosingBracket);
-                        self.reversed_steps.push(Steps::DeleteUntilOpeningBracket);
-                    } else if text[i..].starts_with("\\deleted") {
-                        self.counter_deleted += 1;
-                        self.reversed_steps.push(Steps::DeleteUntilClosingBracket);
-                        self.reversed_steps.push(Steps::DeleteUntilOpeningBracket);
-                    } else if text[i..].starts_with("\\replaced") {
-                        self.counter_replaced += 1;
-                        self.reversed_steps.push(Steps::DeleteUntilClosingBracket);
-                        self.reversed_steps.push(Steps::DeleteUntilOpeningBracket);
-                        self.reversed_steps.push(Steps::KeepUntilClosingBracket);
-                        self.reversed_steps.push(Steps::DeleteUntilOpeningBracket);
-                    } else {
+                    if !self.create_steps_for_command(&text[i..]) {
                         out.push(c);
                     }
                 }
                 Some(Steps::DeleteUntilOpeningBracket) => {
                     if c != '{' {
                         self.reversed_steps.push(step.unwrap());
+                    } else {
+                        self.open_brackets = 1;
                     }
-                    self.open_brackets = 0;
                 }
                 Some(Steps::KeepUntilClosingBracket) => {
-                    if c == '{' {
-                        self.open_brackets += 1;
-                    } else if c == '}' {
-                        if self.open_brackets == 0 {
-                            continue; //mission completed
-                        }
-                        self.open_brackets -= 1;
+                    if self.count_open_brackets(c) != 0 {
+                        self.reversed_steps.push(step.unwrap());
+                        out.push(c);
                     }
-                    self.reversed_steps.push(step.unwrap());
-                    out.push(c);
                 }
                 Some(Steps::DeleteUntilClosingBracket) => {
-                    if c == '{' {
-                        self.open_brackets += 1;
-                    } else if c == '}' {
-                        if self.open_brackets == 0 {
-                            continue; //mission completed
-                        }
-                        self.open_brackets -= 1;
+                    if self.count_open_brackets(c) != 0 {
+                        self.reversed_steps.push(step.unwrap());
                     }
-                    self.reversed_steps.push(step.unwrap());
                 }
             }
         }
         out
     }
 
+    /// If the slice s starts with 'changes' command, then:
+    ///     - create steps for the command processing
+    ///     - increase counter
+    ///     - return true
+    ///
+    /// Possible commands: "\added", "\deleted", "\replaced"
+    fn create_steps_for_command(&mut self, s: &str) -> bool {
+        if s.starts_with("\\added") {
+            self.counter_added += 1;
+            self.reversed_steps.push(Steps::KeepUntilClosingBracket);
+            self.reversed_steps.push(Steps::DeleteUntilOpeningBracket);
+            return true;
+        } else if s.starts_with("\\deleted") {
+            self.counter_deleted += 1;
+            self.reversed_steps.push(Steps::DeleteUntilClosingBracket);
+            self.reversed_steps.push(Steps::DeleteUntilOpeningBracket);
+            return true;
+        } else if s.starts_with("\\replaced") {
+            self.counter_replaced += 1;
+            self.reversed_steps.push(Steps::DeleteUntilClosingBracket);
+            self.reversed_steps.push(Steps::DeleteUntilOpeningBracket);
+            self.reversed_steps.push(Steps::KeepUntilClosingBracket);
+            self.reversed_steps.push(Steps::DeleteUntilOpeningBracket);
+            return true;
+        }
+        false
+    }
+
+    /// Count number of open brackets in a sequence.
+    fn count_open_brackets(&mut self, c: char) -> usize {
+        match c {
+            '{' => self.open_brackets += 1,
+            '}' if self.open_brackets == 0 => {}
+            '}' => self.open_brackets -= 1,
+            _ => {}
+        }
+        self.open_brackets
+    }
+
+    /// Read input file line by line and write processed line into output.
+    /// Input and output file must not be equal.
     pub fn process_file(&mut self, input_file: &Path, output_file: &Path) -> Result<(), Error> {
         use std::io::{BufReader, BufWriter, BufRead, Write};
 
@@ -100,59 +128,3 @@ impl Filter {
         Ok(())
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_defaults() {
-        let f = Filter { ..Default::default() };
-        assert_eq!(f.counter_added, 0);
-        assert_eq!(f.counter_replaced, 0);
-        assert_eq!(f.counter_deleted, 0);
-    }
-
-    #[test]
-    fn test_added() {
-        let mut f = Filter { ..Default::default() };
-        assert_eq!(f.process("\\added{added_text}"), "added_text");
-        assert_eq!(f.process("asdf\\added{added_text}adsf"), "asdfadded_textadsf");
-        assert_eq!(f.process("asdf \\added{added_text}adsf"), "asdf added_textadsf");
-        assert_eq!(f.process("asdf \\add ed{added_text}adsf"), "asdf \\add ed{added_text}adsf");
-        assert_eq!(f.process("asdf \\added{added{}_text\\italic{adfs}}adsf"), "asdf added{}_text\\italic{adfs}adsf");
-        assert_eq!(f.process("\\added{added_text}\\added{added_text}"), "added_textadded_text");
-        assert_eq!(f.counter_added, 6);
-    }
-
-    #[test]
-    fn test_deleted() {
-        let mut f = Filter { ..Default::default() };
-        assert_eq!(f.process("\\deleted{wrong text}"), "");
-        assert_eq!(f.process("\\deleted{wrong text}goal"), "goal");
-        assert_eq!(f.process("goal\\deleted{wrong text}"), "goal");
-        assert_eq!(f.process("go\\deleted{wrong text}al"), "goal");
-        assert_eq!(f.process("\\deleted{wrong \\textit{another of \\textbf{another}} text}goal"), "goal");
-        assert_eq!(f.counter_deleted, 5);
-    }
-
-    //
-    #[test]
-    fn test_replaced() {
-        let mut f = Filter { ..Default::default() };
-        assert_eq!(f.process("\\replaced{good}{wrong}"), "good");
-        assert_eq!(f.process("\\replaced{goo}{wrong}d"), "good");
-        assert_eq!(f.process("g\\replaced{ood}{wrong}"), "good");
-        assert_eq!(f.process("\\replaced{good}{wr\\textit{asdf}ong}"), "good");
-        assert_eq!(f.process("\\replaced{g\\textit{asd}ood}{wr\\textit{asdf}ong}"), "g\\textit{asd}ood");
-        assert_eq!(f.counter_replaced, 5);
-    }
-
-    #[test]
-    fn test_process_file_same_file() {
-        let mut f = Filter::new();
-        let out = f.process_file(Path::new("asdf"), Path::new("asdf"));
-        assert!(out.is_err());
-    }
-}
-
---
+++ src/tests.rs
@@ -0,0 +1,78 @@
+use super::*;
+
+#[test]
+fn test_defaults() {
+    let f = Filter { ..Default::default() };
+    assert_eq!(f.counter_added, 0);
+    assert_eq!(f.counter_replaced, 0);
+    assert_eq!(f.counter_deleted, 0);
+}
+
+#[test]
+fn test_added() {
+    let mut f = Filter { ..Default::default() };
+    assert_eq!(f.process("\\added{added_text}"), "added_text");
+    assert_eq!(f.process("asdf\\added{added_text}adsf"), "asdfadded_textadsf");
+    assert_eq!(f.process("asdf \\added{added_text}adsf"), "asdf added_textadsf");
+    assert_eq!(f.process("asdf \\add ed{added_text}adsf"), "asdf \\add ed{added_text}adsf");
+    assert_eq!(f.process("asdf \\added{added{}_text\\italic{adfs}}adsf"), "asdf added{}_text\\italic{adfs}adsf");
+    assert_eq!(f.process("\\added{added_text}\\added{added_text}"), "added_textadded_text");
+    assert_eq!(f.counter_added, 6);
+}
+
+#[test]
+fn test_deleted() {
+    let mut f = Filter { ..Default::default() };
+    assert_eq!(f.process("\\deleted{wrong text}"), "");
+    assert_eq!(f.process("\\deleted{wrong text}goal"), "goal");
+    assert_eq!(f.process("goal\\deleted{wrong text}"), "goal");
+    assert_eq!(f.process("go\\deleted{wrong text}al"), "goal");
+    assert_eq!(f.process("\\deleted{wrong \\textit{another of \\textbf{another}} text}goal"), "goal");
+    assert_eq!(f.counter_deleted, 5);
+}
+
+#[test]
+fn test_replaced() {
+    let mut f = Filter { ..Default::default() };
+    assert_eq!(f.process("\\replaced{good}{wrong}"), "good");
+    assert_eq!(f.process("\\replaced{goo}{wrong}d"), "good");
+    assert_eq!(f.process("g\\replaced{ood}{wrong}"), "good");
+    assert_eq!(f.process("\\replaced{good}{wr\\textit{asdf}ong}"), "good");
+    assert_eq!(f.process("\\replaced{g\\textit{asd}ood}{wr\\textit{asdf}ong}"), "g\\textit{asd}ood");
+    assert_eq!(f.counter_replaced, 5);
+}
+
+#[test]
+fn test_process_file_same_file() {
+    let mut f = Filter::new();
+    let out = f.process_file(Path::new("asdf"), Path::new("asdf"));
+    assert!(out.is_err());
+}
+
+#[test]
+fn test_count_open_brackets() {
+    let mut f = Filter::new();
+    assert_eq!(f.count_open_brackets('a'), 0);
+    assert_eq!(f.count_open_brackets('{'), 1);
+    assert_eq!(f.count_open_brackets('{'), 2);
+    assert_eq!(f.count_open_brackets('}'), 1);
+    assert_eq!(f.count_open_brackets('{'), 2);
+    assert_eq!(f.count_open_brackets('{'), 3);
+    assert_eq!(f.count_open_brackets('}'), 2);
+    assert_eq!(f.count_open_brackets('}'), 1);
+    assert_eq!(f.count_open_brackets('}'), 0);
+}
+
+#[test]
+fn test_create_steps_for_command() {
+    let mut f = Filter::new();
+    assert_eq!(f.create_steps_for_command("\\adaded"), false);
+    assert_eq!(f.counter_added, 0);
+    assert_eq!(f.create_steps_for_command("\\added"), true);
+    assert_eq!(f.counter_added, 1);
+    assert_eq!(f.create_steps_for_command("\\deleted"), true);
+    assert_eq!(f.create_steps_for_command("\\deleted"), true);
+    assert_eq!(f.counter_deleted, 2);
+    assert_eq!(f.create_steps_for_command("\\replaced"), true);
+    assert_eq!(f.counter_replaced, 1);
+}
